5e2b0009f43a5c00fe411743fc238067d34d346b
tommyno <tommy@netliferesearch.com>
2016-10-17 16:34:24 +0200

    r

--- app.js
+++ app.js
@@ -5,6 +5,17 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
+app.get('/webhook', function(req, res) {
+  if (req.query['hub.mode'] === 'subscribe' &&
+      req.query['hub.verify_token'] === VALIDATION_TOKEN) {
+    console.log("Validating webhook");
+    res.status(200).send(req.query['hub.challenge']);
+  } else {
+    console.error("Failed validation. Make sure the validation tokens match.");
+    res.sendStatus(403);
+  }
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
